7df85232d91ab425e14f432993478409f2d68742
fairoldi <fabio.airoldi@gmail.com>
2018-07-10 00:57:21 -0400

    New recipe

--- js/app.js
+++ js/app.js
@@ -13,6 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
   let instances = M.Parallax.init(elems);
 });
 
+/*
+  Recipe
+*/
 let elems = document.querySelectorAll('input.step');
 elems.forEach(elem => {
   elem.addEventListener('change', event => {
@@ -23,5 +26,43 @@ elems.forEach(elem => {
     } else {
       classes.add('black-text');
     }
+  });
+});
+
+/*
+  new
+*/
+document.addEventListener('DOMContentLoaded', function() {
+  let foodType = document.querySelectorAll('#foodtype');
+  let foodTypeInstance = M.Chips.init(foodType, {
+    autocompleteOptions: {
+      data: {
+        Meat: null,
+        Fish: null,
+        Vegetable: null,
+        Breakfast: null,
+        Dessert: null
+      },
+      limit: Infinity,
+      minLength: 2
+    },
+    placeholder: 'Food type',
+    secondaryPlaceholder: '+type'
+  });
+
+  let cookingMethod = document.querySelectorAll('#cookingmethod');
+  let cookingMethodInstance = M.Chips.init(cookingMethod, {
+    autocompleteOptions: {
+      data: {
+        Stovetop: null,
+        Oven: null,
+        Grill: null,
+        'No Cooking': null
+      },
+      limit: Infinity,
+      minLength: 1
+    },
+    placeholder: 'Cooking method',
+    secondaryPlaceholder: '+method'
   });
 });
