78b7d3d29d1939222a5c68f2e69935afc42e7802
fairoldi <fabio.airoldi@gmail.com>
2018-07-09 22:25:33 -0400

    Recipe page

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
   let elems = document.querySelectorAll('.sidenav');
   let instances = M.Sidenav.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.parallax');
+  let instances = M.Parallax.init(elems);
+});
+
+let elems = document.querySelectorAll('input.step');
+elems.forEach(elem => {
+  elem.addEventListener('change', event => {
+    let label = event.target.labels[0];
+    let classes = label.classList;
+    if (event.target.checked) {
+      classes.remove('black-text');
+    } else {
+      classes.add('black-text');
+    }
+  });
+});
